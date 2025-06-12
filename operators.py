a40b8c150671ad2b7a7dc6b4ef1bf616f9e4ea8e
DevaTharshini0 <dharshinidharshuu525@gmail.com>
2025-06-12 17:38:43 +0530

    operator

---
+++ operators.py
@@ -0,0 +1,100 @@
+#1)
+ a=10
+ b=12
+  c=5
+print(a&b&c)
+#ouput 
+   False
+
+#2)
+a=10
+b=12
+c=5
+print(a+b,a//c,a*b,b%c)
+#ouput
+   22 2 120 2
+
+#3)
+  a=10
+b=12
+c=5
+print(a==b,a!=b,a<b,b>=c)
+#ouput 
+   False True True True
+
+#4)
+ print((a>13)and<b<10)or(a<b)and(b>c))
+
+#output
+    True
+
+#5)
+  print(not(a>15 and c>10) or(<a<b and  b<c))
+
+#output 
+  True
+
+#6)
+
+a=10
+b=12
+c=5
+print(a<<2,a/c,b>>3,a%b)
+#ouput
+    40 15 1 10
+#7)
+  print(a<<2,c>>2,a&b)
+  #output
+      40 1 8
+#8)
+   a=10
+   b=12
+   c=5
+  print(a<=10,b>c,b==5)
+  #output
+     True True False
+#9)
+   print((a>=b)or(b==15) and (b<c)or (a<=b))
+   #output
+     True
+#10)
+  x=10
+  y=12
+  z=5
+ print((x>=y) and (y==15) or(y<z)and(x<=y)
+ #output
+   False
+
+#11
+  x=10
+  y=12
+  z=5
+ print(x^y,y&z,x^z,z&x)
+#output
+ 6 4 15 0
+
+#12)
+  a=10
+  b=12
+  c=-5
+   print(a-b,b+c,a/c,a%b)
+#output
+  -2 10 -2.0 10
+
+#13)
+ print("Maha+deva","Maha","Deva")
+#output
+  Maha+deva MahaDeva
+
+#14)
+ a=10
+b=12
+c=4
+ print(not(a>5)and(b<c))
+#output
+  False
+#15) 
+ print(a<<3)and(b>>3)
+ #output
+  80
+
